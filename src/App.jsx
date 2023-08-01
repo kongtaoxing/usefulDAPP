6fed65d3c556ca65e1ff01b8e0e0ea9879a85d1b
kongtaoxing <xyf8658@163.com>
2023-08-01 19:06:32 +0800

    starkwar

--- src/App.jsx
+++ src/App.jsx
@@ -1,103 +1,105 @@
 import React, { useEffect, useState } from "react";
-import {ethers} from "ethers";
+// import { ethers } from "ethers";
+import {
+	connect,
+	disconnect,
+  } from "get-starknet";
+import { constants, uint256, stark, Contract } from "starknet";
 import "./App.css";
+import etherAbi from "./utils/ether.json";
+import caAbi from "./utils/ca.json";
 import twitterLogo from "./assets/twitter-logo.svg";
-import opNFT from "./assets/opNFT.gif";
-
-const getEthereumObject = () => window.ethereum;
-
-const abi = [
-	"function call(uint256) external payable",
-];
-
-/*
- * This function returns the first linked account found.
- * If there is no account linked, it will return null.
- */
-const findMetaMaskAccount = async () => {
-	try {
-		const ethereum = getEthereumObject();
-
-		/*
-		 * First make sure we have access to the Ethereum object.
-		 */
-		if (!ethereum) {
-			console.error("Make sure you have Metamask!");
-			return null;
-		}
-
-		console.log("We have the Ethereum object", ethereum);
-		const accounts = await ethereum.request({
-			method: "eth_accounts"
-		});
-
-		if (accounts.length !== 0) {
-			const account = accounts[0];
-			console.log("Found an authorized account:", account);
-			return account;
-		} else {
-			console.error("No authorized account found");
-			return null;
-		}
-	} catch (error) {
-		console.error(error);
-		return null;
-	}
-};
+import warNFT from "./assets/starwar.png";
 
 const App = () => {
 	const [currentAccount, setCurrentAccount] = useState("");
+	const [account, setAccount]= useState(null);
+	const [chain, setChain] = useState("");
 	const [value, setValue] = useState();
 	const [hash, setHash] = useState("");
 
-	const contractAddress = "0x09a402402612e529c29809197a78662db18cb038";
+	const etherContract = "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7";
+	const contractAddress = "0x17bcbb8fca1ca93ed677a88609682106e2a1104bbffb7749a25afe837a77f21";
 
 	const connectWallet = async () => {
+		const windowStarknet = await connect({
+		  modalMode: "alwaysAsk"
+		})
+		await windowStarknet?.enable({ starknetVersion: "v4" })
+		return windowStarknet;
+	}
+
+	const handleConnectClick = async () => {
 		try {
-			const ethereum = getEthereumObject();
-			if (!ethereum) {
-				alert("Get MetaMask!");
-				return;
+		  const wallet = await connectWallet()
+		  if (wallet?.account) {
+			setAccount(() => wallet.account);
+			setCurrentAccount(() => wallet.account.address);
+			let chainName = await chainId(wallet?.provider.chainId);
+			setChain(() => chainName);
+			// setConnected(connected => {return !!wallet?.isConnected})
+			console.log('current account:', currentAccount);
+			if (chainName != "mainnet-alpha") {
+				console.log("切换到主网");
 			}
-
-			const accounts = await ethereum.request({
-				method: "eth_requestAccounts",
-			});
-
-			console.log("Connected", accounts[0]);
-			setCurrentAccount(accounts[0]);
-		} catch (error) {
-			console.error(error);
+		  }
 		}
-	};
+		catch(e) {
+		  console.log(e.message);
+		  if (e.message.includes('User abort')) { /*ignore*/ }
+		}
+	  }
+
+	async function chainId(id) {
+		if (id === constants.StarknetChainId.MAINNET) {
+		  return "mainnet-alpha"
+		} else if (id === constants.StarknetChainId.TESTNET) {
+		  return "goerli-alpha"
+		} else {
+		  return "localhost"
+		}
+	}
 
 	const _mint = async () => {
-		try {
-			const { ethereum } = window;
+		if (currentAccount) {
+			const callEther = new Contract(etherAbi, etherContract, account);
+			const callContract = new Contract(caAbi, contractAddress, account);
 
-			if (ethereum) {
-				const provider = new ethers.providers.Web3Provider(ethereum);
-				const signer = provider.getSigner();
-				const callContract = new ethers.Contract(contractAddress, abi, signer);
+			/*
+				* Execute the actual call from your smart contract
+				*/
+			const approve = [{
+				contractAddress: etherContract,
+				entrypoint: "approve",
+				calldata: [contractAddress, String(500000000000000 * value), "0"]
+			}];
+			const call = {
+				contractAddress: contractAddress,
+				entrypoint: "mint_a",
+				calldata: ["0x0746f97FbD3C65916Ee7024Bf5a6cF6a813aab13dC7793633D7159508170A84E"]
+			};
+			const call_string = JSON.stringify(call) + ',';
+			const call_raw = '[' + call_string.repeat(value) + ']';
+			const fee = [
+				{
+					contractAddress: etherContract,
+					entrypoint: "transfer",
+					calldata: ["0x0746f97FbD3C65916Ee7024Bf5a6cF6a813aab13dC7793633D7159508170A84E", "500000000000000", "0"]
+				}
+			]
+			const callData = approve.concat(eval(call_raw)).concat(fee);
+			const callTxn = await account.execute(
+				callData
+			)
+			console.log("Mining...", callTxn.transaction_hash);
 
-				/*
-				 * Execute the actual call from your smart contract
-				 */
-				const callTxn = await callContract.call(value, {
-					value: ethers.utils.parseUnits("0.001", "ether")
-				});
-				console.log("Mining...", callTxn.hash);
+			setHash(() => "https://starkscan.co/tx/" + callTxn.hash);
 
-				setHash(() => "https://optimistic.etherscan.io/tx/" + callTxn.hash);
-
-				await callTxn.wait();
-				console.log("Mined -- ", callTxn.hash);
-				console.log("Minted successfully");
-			} else {
-				console.log("Ethereum object doesn't exist!");
-			}
-		} catch (error) {
-			console.log(error);
+			await callTxn.wait();
+			console.log("Mined -- ", callTxn.hash);
+			console.log("Minted successfully");
+		} else {
+			console.log("Ethereum object doesn't exist!");
 		}
 	}
 
@@ -106,10 +108,10 @@ const App = () => {
 	 * More technically, when the App component "mounts".
 	 */
 	useEffect(async () => {
-		const account = await findMetaMaskAccount();
-		if (account !== null) {
-			setCurrentAccount(account);
-		}
+		// const account = await findMetaMaskAccount();
+		// if (account !== null) {
+		// 	setCurrentAccount(account);
+		// }
 	}, []);
 
 	return (
@@ -117,19 +119,19 @@ const App = () => {
 			<div className="mainContainer">
 				<div className="dataContainer">
 					<div className="header">
-						mint.fun Op链纪念NFT铸造
+						Starkwars NFT铸造
 					</div>
 
 					<div className="bio">
-						输入铸造数量，然后点击"mint"铸造，喜欢的话欢迎关注我的<a href="https://twitter.com/kongtaoxing">推特</a>，开源代码：
-						<a href="https://github.com/kongtaoxing/usefulDAPP/tree/opMintFun">usefulDAPP</a>
+						输入铸造数量(最多5个)，然后点击"mint"铸造，喜欢的话欢迎关注我的<a href="https://twitter.com/kongtaoxing">推特</a>，开源代码：
+						<a href="https://github.com/kongtaoxing/usefulDAPP/tree/starkwars">usefulDAPP</a>
 					</div>
 
 					{/*
 					* If there is no currentAccount render this button
 					*/}
 					{!currentAccount && (
-					<button className="callButton" onClick={connectWallet}>
+					<button className="callButton" onClick={handleConnectClick}>
 						Connect Wallet
 					</button>
 					)}
@@ -138,23 +140,23 @@ const App = () => {
 						<span className="grid-item">
               				请输入想要mint的数量：
             			</span>
-						<input type="text" value={value} placeholder = '100' style={{borderRadius:'4px',border:'none'}} onChange={a=>{setValue(a.target.value)}} />
+						<input type="text" value={value} placeholder = '5' style={{borderRadius:'4px',border:'none'}} onChange={a=>{setValue(a.target.value)}} />
 					</div>
 					
-					<button className="callButton" onClick={_mint} disabled> 
-						Mint（已结束）
+					<button className="callButton" onClick={_mint}> 
+						Mint
 					</button>
 
 					{
 						hash
 						&&
-						<a href={hash} className="bio">铸造哈希：https://optimistic.etherscan.io/</a>
+						<a href={hash} className="bio">铸造哈希：https://starkscan.co/tx/</a>
 					}
 
 					<br></br>
 
 					{/* <video src={baseNFT} autoPlay loop /> */}
-					<img src={opNFT} />
+					<img src={warNFT} />
 
 					<div className="footer-container">
 						<img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
